4a8c00f9108d77b7029faf8a3ab3abe0621e7383
Mainard Solis <89048194+mainardsolis@users.noreply.github.com>
2024-06-16 14:35:16 -0300

    Delete ecommerce directory

--- ecommerce/store/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
-def home (request):
-	products = Product.objects.all()
-	return render(request, 'home.html', {'products':products})
